ef337d826c4aec53b4a0348aedeb8e803ce68ef9
andylau <1173602856@qq.com>
2025-11-27 17:16:57 +0800

    debug

--- criterion/ctx_loss.py
+++ criterion/ctx_loss.py
@@ -12,6 +12,7 @@ class CtxLoss(nn.Module):
         img_pred: B,3,H,W
         img_gt: B,3,H,W
         """
+        img_gt = F.avg_pool2d(img_gt,4,4)
         img_gt = (img_gt - img_gt.min()) / (img_gt.max() - img_gt.min())
         ctx_loss = torch.abs(img_pred - img_gt).mean()
         return ctx_loss
