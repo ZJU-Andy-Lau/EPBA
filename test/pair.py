820a2e2fb3920c5b3384c29fa3dd24d0997c5774
andylau <1173602856@qq.com>
2025-12-03 20:06:49 +0800

    debug

--- test/pair.py
+++ test/pair.py
@@ -110,6 +110,7 @@ class Solver():
         corners_linesamps_b = self.rs_image_b.convert_diags_to_corners(diags,self.rpc_b)
         
         imgs_a,dems_a,Hs_a = self.rs_image_a.crop_windows(corners_linesamps_a)
+        print(imgs_a)
         imgs_b,dems_b,Hs_b = self.rs_image_b.crop_windows(corners_linesamps_b)
         
         return (imgs_a,dems_a,Hs_a),(imgs_b,dems_b,Hs_b)
