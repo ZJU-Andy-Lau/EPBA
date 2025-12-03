ae2b07bf7740b9ffa641b3fcbc70f6cd87bb262a
andylau <1173602856@qq.com>
2025-12-03 19:58:08 +0800

    debug

--- test/window.py
+++ test/window.py
@@ -13,12 +13,12 @@ from shared.rpc import RPCModelParameterTorch
 
 
 class Window():
-    def __init__(self,img:np.ndarray,dem:np.ndarray,rpc:RPCModelParameterTorch,H:torch.Tensor):
+    def __init__(self,img:np.ndarray,dem:np.ndarray,rpc:RPCModelParameterTorch,H:np.ndarray):
         self.img = img,
         self.dem = dem
         self.rpc = rpc
         self.H = H
-        self.H_inv = torch.linalg.inv(H)
+        self.H_inv = np.linalg.inv(H)
         self.match_feats,self.ctx_feats,self.confs = None,None,None
     
     def load_feats(self,feats:feats_type):
