59167137ae3261cddffbb2ffa155fa0d73152d9e
andylau <1173602856@qq.com>
2025-12-09 12:16:13 +0800

    debug

--- preprocess/CasP/registration.py
+++ preprocess/CasP/registration.py
@@ -300,8 +300,7 @@ class ImageRegistrar:
             # ---------------------------
             # [新增功能] 3. 生成并保存棋盘格对比图
             # ---------------------------
-            print(ref_orig.shape,warped_img.shape)
-            checkerboard_img = make_checkerboard(ref_orig, warped_img, 15)
+            checkerboard_img = make_checkerboard(ref_orig[:,:,0], warped_img, 15)
             checkerboard_filename = f"checkerboard_{i}.jpg"
             checkerboard_path = os.path.join(output_dir, checkerboard_filename)
             cv2.imwrite(checkerboard_path, cv2.cvtColor(checkerboard_img, cv2.COLOR_RGB2BGR))
