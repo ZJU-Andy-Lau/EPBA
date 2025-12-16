637b7f0fb915b609c024d9e174b7ce3269562598
andylau <1173602856@qq.com>
2025-12-16 20:49:59 +0800

    debug

--- solve/test_global_affine_solver.py
+++ solve/test_global_affine_solver.py
@@ -82,7 +82,7 @@ def run_test_with_provided_data(images: List[RSImage], pair_results: List[dict])
     device = images[0].device
     
     # 实例化求解器
-    solver = GlobalAffineSolver(images, device=device,anchor_indices=[1])
+    solver = GlobalAffineSolver(images, device=device,anchor_indices=[2])
     
     # 求解
     Ms = solver.solve(pair_results)
