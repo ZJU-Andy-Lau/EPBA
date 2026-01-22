8b4b3b3bfc2209dadfe0da07d4c95fc5f1a88022
andylau <1173602856@qq.com>
2026-01-22 19:16:02 +0800

    debug

--- baseline/matchers/roma.py
+++ baseline/matchers/roma.py
@@ -52,8 +52,10 @@ class RoMaMatcher(BaseMatcher):
 
     def match(self, img_a: np.ndarray, img_b: np.ndarray) -> MatchResult:
         start = time.perf_counter()
-        img_a = torch.from_numpy(img_a).to(self.device).unsqueeze(0)
-        img_b = torch.from_numpy(img_b).to(self.device).unsqueeze(0)
+        img_a = np.stack([img_a] * 3,axis=0)[None]
+        img_b = np.stack([img_b] * 3,axis=0)[None]
+        img_a = torch.from_numpy(img_a).to(self.device)
+        img_b = torch.from_numpy(img_b).to(self.device)
         warp, certainty = self.model.match(img_a, img_b, device=self.device)
         matches, certainty = self.model.sample(warp, certainty)
         if matches is None or len(matches) == 0:
